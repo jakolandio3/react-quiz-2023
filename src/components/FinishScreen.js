067cd081c4071651e2f83b087f80e627a21aeb6f
Jakob Douglas <jakobdouglas1@gmail.com>
2023-09-07 10:42:31 +0930

    fix: fixed issue where total points variable was incorrect causing a NaN response

--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { useQuiz } from '../context/QuizContext';
 
 export default function FinishScreen() {
-	const { points, totalScore, highScore, dispatch } = useQuiz();
-	const percentage = (points / totalScore) * 100;
+	const { points, totalPoints, highScore, dispatch } = useQuiz();
+	const percentage = (+points / +totalPoints) * 100;
 
 	let emoji;
 	if (percentage === 100) emoji = '🏅';
@@ -14,8 +14,8 @@ export default function FinishScreen() {
 	return (
 		<>
 			<p className='result'>
-				<span>{emoji} </span> You scored <strong> {points} </strong>/{' '}
-				{totalScore} ({Math.ceil(percentage)}%)
+				<span>{emoji} </span> You scored <strong> {points} </strong>/
+				{totalPoints} ({Math.ceil(percentage)}%)
 			</p>
 			<p className='highscore'>(highscore: {highScore} points)</p>
 			<button
